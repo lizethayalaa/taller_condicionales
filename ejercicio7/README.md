c7a0aead2730625f5aada9858be7b1a593ec114d
sistemas <you@example.com>
2023-03-15 08:41:57 -0500

    se creo el diagrama de flujo

---
+++ ejercicio7/README.md
@@ -0,0 +1,5 @@
+# Taller_condicionales
+
+
+# Diagrama de flujo
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
---
+++ ejercicio7/ejercicio7.py
@@ -0,0 +1,11 @@
+# Pedir los tres numero enteros
+num1 = int(input("ingrese el primer numero entero: "))
+num2 = int(input("inrese el segundo numero entero: "))
+num3 = int(input("ingrese el tercer numero entero: "))
+
+# Verificar si la suma de los dos primeros numeros es igual al tercero
+if num1 + num2 == num3:
+    print("la suma de los dos primeros numeros es igual al tercero")
+else: 
+    print("la suma de los dos primeros numeros no es igualn al tercero.")    
+    
