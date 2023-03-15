5c3a4e3a702c7499ce75e91feaf152ccb3aad809
sistemas <you@example.com>
2023-03-15 08:27:36 -0500

    se creo el diagrama de flujo

---
+++ ejercicio6/ejercicio6.py
@@ -0,0 +1,29 @@
+
+
+#input
+
+import math
+
+
+a = int(input("se agrega el valor de a: "))
+b = int(input("se agrega el valor de b: "))
+c = int(input("se agrega el valor de c: "))
+
+#Processing
+
+d=b**2-4*a*c
+
+if d == 0:
+    x1=(b/2*a)
+    x2=x1
+    print(x1, x2)
+
+
+if d>0:
+       x1=(-b*math.sqrt(d))/(2*a) 
+       x2=(-b-math.sqtr(d))/(2*a)
+       print(x1, x2)
+
+else:
+       print("las raices son imginarias") 
+             
